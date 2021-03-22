66a2d9fd2e5d34be8b7edb57f7bc1e63327b215f
saqib <saqib26ece@gmail.com>
2021-03-22 12:04:46 +0530

    New commit

--- my_project/src/main/java/com/example/Greeter.java
+++ my_project/src/main/java/com/example/Greeter.java
@@ -21,3 +21,19 @@ public class Greeter {
   }
 }
 
+
+
+
+
+ public Greeter() {
+
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+    public String greet2(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
