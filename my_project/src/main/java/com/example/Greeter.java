8e3a8415bf58517cbf5aafbae33a4776112b91af
saqib <saqib26ece@gmail.com>
2021-03-23 22:02:05 +0530

    demo project3

--- my_project/src/main/java/com/example/Greeter.java
+++ my_project/src/main/java/com/example/Greeter.java
@@ -10,16 +10,15 @@ public class Greeter {
    */
   public Greeter() {
 
-  }
+
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
-  public String greet2(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+    }
   
   
+}
 }
 
 
