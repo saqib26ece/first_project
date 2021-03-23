00d08247324c8414c5532ac58719decc28047933
saqib <saqib26ece@gmail.com>
2021-03-23 22:05:25 +0530

    demo project4

--- my_project/src/main/java/com/example/Greeter.java
+++ my_project/src/main/java/com/example/Greeter.java
@@ -10,19 +10,10 @@ public class Greeter {
    */
   public Greeter() {
 
-
+  }
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
-    }
-  
-  
+  }
 }
-}
-
-
-
-
-
-
