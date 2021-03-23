00a33fec0632c597907ea2660dccf0e35d0e6e59
saqib <saqib26ece@gmail.com>
2021-03-23 22:12:33 +0530

    demo project5

--- my_project/src/main/java/com/example/Greeter.java
+++ my_project/src/main/java/com/example/Greeter.java
@@ -16,4 +16,7 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
+   public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
 }
