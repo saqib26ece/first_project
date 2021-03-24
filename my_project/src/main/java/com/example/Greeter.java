23f3d91ad7ce77afe6d69ed0fcfc0bc3aac4b15e
saqib <saqib26ece@gmail.com>
2021-03-24 16:52:44 +0530

    demo-2

--- my_project/src/main/java/com/example/Greeter.java
+++ my_project/src/main/java/com/example/Greeter.java
@@ -16,26 +16,4 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone) ret:::;urn String.format("Hello, %s!", someone);
-  }
-}
-
-
-public class Greeter {
-
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
-
-  }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone) ret:::;urn String.format("Hello, %s!", someone);
-  }
 }
