8fef97555b3185fa8b0432a878df1cdf354c38ad
saqib <saqib26ece@gmail.com>
2021-03-24 13:55:25 +0530

    Commit for demo2

--- my_project/src/main/java/com/example/Greeter.java
+++ my_project/src/main/java/com/example/Greeter.java
@@ -16,7 +16,4 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
-   public String greet2(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
 }
